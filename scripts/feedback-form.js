008df5f30fae9b1ef1840633d2011a886aacc68b
Alice Eldridge <alice@ecila.org>
2024-03-12 11:35:43 +0000

    Update feedback-form.js
    broke the submit button - trying to see why

--- scripts/feedback-form.js
+++ scripts/feedback-form.js
@@ -17,11 +17,10 @@ function sendfeedbackemail(submitElement) {
 	feedbackBody = document.getElementsByClassName("feedback__body")[0].value;
 	feedbackName = document.getElementsByClassName("feedback__name")[0].value;
 	feedbackEmail = document.getElementsByClassName("feedback__email")[0].value;
-
 	researchName = document.getElementsByClassName("research__name")[0].value;
 	researchEmail = document.getElementsByClassName("research__email")[0].value;
 
-	var thebody = feedbackName + "<br><br>"+feedbackEmail+"<br><br>"+ researchName + "<br><br>"+ researchEmail + "<br><br>"+feedbackBody.replace(/(?:\r\n|\r|\n)/g, '<br>') 
+	var thebody = feedbackName + "<br><br>"+feedbackEmail+"<br><br>"+ researchName + "<br><br>"+ researchEmail + "<br><br>"+feedbackBody.replace(/(?:\r\n|\r|\n)/g, '<br>')
 
 	Email.send({
 	    SecureToken : "b13b383d-38d6-44ad-9e7b-3618a9874657",
