e6560d390c73dc4df6de6ac982d2192d1671c04a
maxbaraitsersmith <116178119+maxbaraitsersmith@users.noreply.github.com>
2024-07-17 10:07:27 +0200

    Update feedback-form.js

--- scripts/feedback-form.js
+++ scripts/feedback-form.js
@@ -3,7 +3,9 @@ var feedbackBody;
 var feedbackName;
 var feedbackEmail;
 
-function confirmSent(submitElement) {
+function confirmSent(submitElement,thebody) {
+	console.log("Sent email");
+	console.log(thebody);
 	submitElement.classList.remove("feedback__submit");
 	submitElement.innerHTML = "Thanks for giving your feedback!"
 	feedbackForm.reset();
@@ -14,17 +16,21 @@ function sendfeedbackemail(submitElement) {
 	feedbackBody = document.getElementsByClassName("feedback__body")[0].value;
 	feedbackName = document.getElementsByClassName("feedback__name")[0].value;
 	feedbackEmail = document.getElementsByClassName("feedback__email")[0].value;
+	feedbackNameFutureResearch = document.getElementsByClassName("feedback__name_future_research")[0].value;
+	feedbackEmailFutureResearch = document.getElementsByClassName("feedback__email_future_research")[0].value;
 
-	var thebody = feedbackName + "<br><br>"+feedbackEmail+"<br><br>"+feedbackBody.replace(/(?:\r\n|\r|\n)/g, '<br>')
+	var thebody = "Name and email:<br><br>"+ feedbackName + "<br><br>"+feedbackEmail+"<br><br> Name and email for future research:<br><br>"+feedbackNameFutureResearch + "<br><br>"+feedbackEmailFutureResearch+"<br><br> Body text:<br><br>"+feedbackBody.replace(/(?:\r\n|\r|\n)/g, '<br>')
 
 	Email.send({
 	    SecureToken : "b13b383d-38d6-44ad-9e7b-3618a9874657",
 	    To : 'wilding.radio@gmail.com',
 	    From : "wilding.radio.feedback@soundtent.org",
-	    Subject : "New wilding.radio feedback ",
+	    Subject : "New wilding.radio feedback",
 	    Body : thebody
 	}).then(
-	  confirmSent(submitElement)
+	confirmSent(submitElement, thebody)
 	);
 }
+
+
 
